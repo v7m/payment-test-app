7b71638c0b0e9edeac420b3fd711db4b920143b6
Vadym Ladimyrych <vadym.ladimyrych@whitebox-services.eu>
2023-11-20 19:45:46 +0100

    Add scopes to Transaction model

--- app/models/Transaction.rb
+++ app/models/Transaction.rb
@@ -11,4 +11,9 @@ class Transaction < ApplicationRecord
   validates :amount, numericality: { greater_than: 0 }, allow_nil: true
   validates :status, inclusion: { in: statuses.keys }, allow_nil: true
   validates :customer_email, presence: true, email_format: true
+
+  scope :approved_authorize, -> { where(type: "Transactions::AuthorizeTransaction").approved }
+  scope :approved_revessed, -> { where(type: "Transactions::AuthorizeTransaction").reversed }
+  scope :charge_authorize, -> { where(type: "Transactions::ChargeTransaction").approved }
+  scope :charge_reversed, -> { where(type: "Transactions::ChargeTransaction").reversed }
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -14,4 +14,40 @@ describe Transaction, type: :model do
     it { is_expected.to validate_presence_of(:customer_email) }
     it { is_expected.to validate_email_format_of(:customer_email) }
   end
+
+  context "for scopes" do
+    let(:merchant) { create(:merchant, :active) }
+    let(:authorize_transaction_approved) { create(:authorize_transaction, :approved, merchant:) }
+    let(:authorize_transaction_reversed) { create(:authorize_transaction, :reversed, merchant:) }
+    let(:charge_transaction_approved) do
+      create(:charge_transaction, :approved, merchant:, referenced_transaction: authorize_transaction_approved)
+    end
+    let(:charge_transaction_reversed) do
+      create(:charge_transaction, :reversed, merchant:, referenced_transaction: authorize_transaction_approved)
+    end
+
+    describe ".approved_authorize" do
+      it "returns only approved authorize transactions" do
+        expect(described_class.approved_authorize).to eq([authorize_transaction_approved])
+      end
+    end
+
+    describe ".approved_revessed" do
+      it "eturns only approved reversed transactions" do
+        expect(described_class.approved_revessed).to eq([authorize_transaction_reversed])
+      end
+    end
+
+    describe ".charge_authorize" do
+      it "returns only approved charge transactions" do
+        expect(described_class.charge_authorize).to eq([charge_transaction_approved])
+      end
+    end
+
+    describe ".charge_reversed" do
+      it "eturns only charge reversed transactions" do
+        expect(described_class.charge_reversed).to eq([charge_transaction_reversed])
+      end
+    end
+  end
 end
